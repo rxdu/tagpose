1e04eaa4ad2767466e74c039625b0f81c0ec4990
rdu <ruixiang.du@gmail.com>
2016-11-01 20:58:09 -0400

    added eigen, updated readme

---
+++ src/pose/apriltag_pose.cpp
@@ -0,0 +1,22 @@
+/*
+ * apriltag_pose.cpp
+ *
+ *  Created on: Nov 1, 2016
+ *      Author: rdu
+ */
+
+#include "apriltag_pose.h"
+
+using namespace apriltag_pose;
+
+AprilTagPose::AprilTagPose()
+{
+
+}
+
+AprilTagPose::~AprilTagPose()
+{
+
+}
+
+
---
+++ src/pose/apriltag_pose.h
@@ -0,0 +1,29 @@
+/*
+ * apriltag_pose.h
+ *
+ *  Created on: Nov 1, 2016
+ *      Author: rdu
+ */
+
+#ifndef POSE_APRILTAG_POSE_H_
+#define POSE_APRILTAG_POSE_H_
+
+#include "opencv2/opencv.hpp"
+
+namespace apriltag_pose {
+
+class AprilTagPose
+{
+public:
+	AprilTagPose();
+	~AprilTagPose();
+
+private:
+
+public:
+
+};
+
+}
+
+#endif /* POSE_APRILTAG_POSE_H_ */
