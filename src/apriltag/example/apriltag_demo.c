fee42da685a891e916a962251b9ad45ed7442cda
rdu <ruixiang.du@gmail.com>
2017-03-09 12:09:31 -0500

    updated apriltag library to version 2016-12-01 (0.9.8)

--- src/apriltag/example/apriltag_demo.c
+++ src/apriltag/example/apriltag_demo.c
@@ -1,12 +1,10 @@
-/* (C) 2013-2016, The Regents of The University of Michigan
+/* Copyright (C) 2013-2016, The Regents of The University of Michigan.
 All rights reserved.
 
 This software was developed in the APRIL Robotics Lab under the
 direction of Edwin Olson, ebolson@umich.edu. This software may be
-available under alternative licensing terms; contact the address
-above.
+available under alternative licensing terms; contact the address above.
 
-   BSD
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
 
@@ -29,14 +27,15 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 The views and conclusions contained in the software and documentation are those
 of the authors and should not be interpreted as representing official policies,
-either expressed or implied, of the FreeBSD Project.
- */
+either expressed or implied, of the Regents of The University of Michigan.
+*/
 
 #include <stdio.h>
 #include <stdint.h>
 #include <inttypes.h>
 #include <ctype.h>
 #include <unistd.h>
+#include <math.h>
 
 #include "apriltag.h"
 #include "tag36h11.h"
@@ -46,6 +45,10 @@ either expressed or implied, of the FreeBSD Project.
 #include "tag25h7.h"
 
 #include "common/getopt.h"
+#include "common/image_u8.h"
+#include "common/image_u8x4.h"
+#include "common/pjpeg.h"
+#include "common/zarray.h"
 
 // Invoke:
 //
@@ -63,7 +66,7 @@ int main(int argc, char *argv[])
     getopt_add_int(getopt, 'i', "iters", "1", "Repeat processing on input set this many times");
     getopt_add_int(getopt, 't', "threads", "4", "Use this many CPU threads");
     getopt_add_double(getopt, 'x', "decimate", "1.0", "Decimate input image by this factor");
-    getopt_add_double(getopt, 'b', "blur", "0.0", "Apply low-pass blur to input");
+    getopt_add_double(getopt, 'b', "blur", "0.0", "Apply low-pass blur to input; negative sharpens");
     getopt_add_bool(getopt, '0', "refine-edges", 1, "Spend more time trying to align edges of tags");
     getopt_add_bool(getopt, '1', "refine-decode", 0, "Spend more time trying to decode tags");
     getopt_add_bool(getopt, '2', "refine-pose", 0, "Spend more time trying to precisely localize tags");
@@ -113,6 +116,11 @@ int main(int argc, char *argv[])
 
     for (int iter = 0; iter < maxiters; iter++) {
 
+        int total_quads = 0;
+        int total_hamm_hist[hamm_hist_max];
+        memset(total_hamm_hist, 0, sizeof(total_hamm_hist));
+        double total_time = 0;
+
         if (maxiters > 1)
             printf("iter %d / %d\n", iter + 1, maxiters);
 
@@ -125,8 +133,55 @@ int main(int argc, char *argv[])
             zarray_get(inputs, input, &path);
             if (!quiet)
                 printf("loading %s\n", path);
+            else
+                printf("%20s ", path);
 
-            image_u8_t *im = image_u8_create_from_pnm(path);
+            image_u8_t *im = NULL;
+            if (str_ends_with(path, "pnm") || str_ends_with(path, "PNM") ||
+                str_ends_with(path, "pgm") || str_ends_with(path, "PGM"))
+                im = image_u8_create_from_pnm(path);
+            else if (str_ends_with(path, "jpg") || str_ends_with(path, "JPG")) {
+                int err = 0;
+                pjpeg_t *pjpeg = pjpeg_create_from_file(path, 0, &err);
+                if (pjpeg == NULL) {
+                    printf("pjpeg error %d\n", err);
+                    continue;
+                }
+
+                if (1) {
+                    im = pjpeg_to_u8_baseline(pjpeg);
+                } else {
+                    printf("illumination invariant\n");
+
+                    image_u8x3_t *imc =  pjpeg_to_u8x3_baseline(pjpeg);
+
+                    im = image_u8_create(imc->width, imc->height);
+
+                    for (int y = 0; y < imc->height; y++) {
+                        for (int x = 0; x < imc->width; x++) {
+                            double r = imc->buf[y*imc->stride + 3*x + 0] / 255.0;
+                            double g = imc->buf[y*imc->stride + 3*x + 1] / 255.0;
+                            double b = imc->buf[y*imc->stride + 3*x + 2] / 255.0;
+
+                            double alpha = 0.42;
+                            double v = 0.5 + log(g) - alpha*log(b) - (1-alpha)*log(r);
+                            int iv = v * 255;
+                            if (iv < 0)
+                                iv = 0;
+                            if (iv > 255)
+                                iv = 255;
+
+                            im->buf[y*im->stride + x] = iv;
+                        }
+                    }
+                    image_u8x3_destroy(imc);
+                    if (td->debug)
+                        image_u8_write_pnm(im, "debug_invariant.pnm");
+                }
+
+                pjpeg_destroy(pjpeg);
+            }
+
             if (im == NULL) {
                 printf("couldn't load %s\n", path);
                 continue;
@@ -143,29 +198,44 @@ int main(int argc, char *argv[])
                            i, det->family->d*det->family->d, det->family->h, det->id, det->hamming, det->goodness, det->decision_margin);
 
                 hamm_hist[det->hamming]++;
+                total_hamm_hist[det->hamming]++;
             }
 
             apriltag_detections_destroy(detections);
 
             if (!quiet) {
                 timeprofile_display(td->tp);
-                printf("nedges: %d, nsegments: %d, nquads: %d\n", td->nedges, td->nsegments, td->nquads);
             }
+
+            total_quads += td->nquads;
 
             if (!quiet)
-                printf("Hamming histogram: ");
+                printf("hamm ");
 
             for (int i = 0; i < hamm_hist_max; i++)
-                printf("%5d", hamm_hist[i]);
+                printf("%5d ", hamm_hist[i]);
 
-            if (quiet) {
-                printf("%12.3f", timeprofile_total_utime(td->tp) / 1.0E3);
-            }
+            double t =  timeprofile_total_utime(td->tp) / 1.0E3;
+            total_time += t;
+            printf("%12.3f ", t);
+            printf("%5d", td->nquads);
 
             printf("\n");
 
             image_u8_destroy(im);
         }
+
+
+        printf("Summary\n");
+
+        printf("hamm ");
+
+        for (int i = 0; i < hamm_hist_max; i++)
+            printf("%5d ", total_hamm_hist[i]);
+        printf("%12.3f ", total_time);
+        printf("%5d", total_quads);
+        printf("\n");
+
     }
 
     // don't deallocate contents of inputs; those are the argv
